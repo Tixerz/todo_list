e30a698486d3a71177726320f2097f8157e109dc
Tixerz <petete1296@gamil.com>
2023-08-09 04:53:55 -0400

    timer tab creation

--- main.py
+++ main.py
@@ -18,13 +18,17 @@ for item in os.listdir() :  #loading the saved objects
 window = tk.Tk()
 
 window.geometry("600x400" )
-
-category_text_label = tk.Label(text="category:" , bg = "#ebbfd0" , font="MathJax_TypeWriter") 
-category_text_label.place( x=0 , y= 10)
+notebook = ttk.Notebook(window)
+notebook.pack()
+objectives = tk.Frame(notebook ,width=600 , height=400 , bg= "#ebbfd0")
+objectives.pack()
+notebook.add(objectives, text="objectives")
+category_text_label = tk.Label(objectives , text="category:" , bg = "#ebbfd0" , font="MathJax_TypeWriter") 
+category_text_label.place( x=0 , y= 30)
 list1 = [item.name for i in category_list] #cat list
 window.configure(bg= "#ebbfd0")
 
-list_box = tk.Listbox(window , bg= "#e9d9db") 
+list_box = tk.Listbox(objectives , bg= "#e9d9db") 
 def delete():
     global list_box
 
@@ -37,38 +41,38 @@ def delete():
     
 
 
-list_box.place(x=32,y=90)
-delete_button = tk.Button(window , command= delete , text= "Delete" , bg="#f57cab" , font="MathJax_TypeWriter")
-delete_button.place(x = 314, y = 160)
-list_box.place(x = 150 , y = 120)
+list_box.place(x=32,y=110)
+delete_button = tk.Button(objectives , command= delete , text= "Delete" , bg="#f57cab" , font="MathJax_TypeWriter")
+delete_button.place(x = 314, y = 180)
+list_box.place(x = 150 , y = 140)
 
 def list_refresh():
     global list_box 
-    list_box = tk.Listbox(window,bg= "#e9d9db") 
+    list_box = tk.Listbox(objectives,bg= "#e9d9db") 
     for item in category_list :
         if item.name == category_list_combobox.get():
             for x in item.data:
                 list_box.insert(0,x)
-    list_box.place(x = 150 , y = 120)
+    list_box.place(x = 150 , y = 140)
     
-refresh_button = tk.Button(window , text= "refresh" , command=list_refresh , bg = "#f57cab" , font="MathJax_TypeWriter")
-refresh_button.place(x= 314, y=120)
+refresh_button = tk.Button(objectives, text= "refresh" , command=list_refresh , bg = "#f57cab" , font="MathJax_TypeWriter")
+refresh_button.place(x= 314, y=140)
 
-item_label = tk.Label(window , text= " item:" , bg = "#ebbfd0" , font="MathJax_TypeWriter")
-item_label.place(x=0 , y = 70)
+item_label = tk.Label(objectives , text= " item:" , bg = "#ebbfd0" , font="MathJax_TypeWriter")
+item_label.place(x=0 , y = 90)
 
-entry = tk.Entry(window ,width = 15 , bg="#e9d9db" , font="MathJax_TypeWriter")
-entry.place( x = 80, y = 70)
-
-
+entry = tk.Entry(objectives ,width = 15 , bg="#e9d9db" , font="MathJax_TypeWriter")
+entry.place( x = 80, y = 90)
 
 
-category_list_combobox = ttk.Combobox(window ,values= list1  ,postcommand=list_refresh  )
+
+
+category_list_combobox = ttk.Combobox(objectives ,values= list1  ,postcommand=list_refresh  )
 style = ttk.Style()
 style.theme_use('clam')
 style.configure("TCombobox", fieldbackground= "#e9d9db", background= "#e9d9db")
 
-category_list_combobox.place(x=80 , y = 10)
+category_list_combobox.place(x=80 , y = 30)
 
 
 def add_cat():
@@ -78,12 +82,12 @@ def add_cat():
     item = category_list_combobox.get()
     category_list.append(category(item , "red"))
     list1 = [i.name for i in category_list]
-    category_list_combobox = ttk.Combobox(window ,values= list1 )
-    category_list_combobox.place(x=80 , y = 10)
+    category_list_combobox = ttk.Combobox(objectives ,values= list1 )
+    category_list_combobox.place(x=80 , y = 30)
     list_refresh()
 
-add_cat_button = tk.Button(window , text= "Add" , command=add_cat , bg="#f57cab" , font="MathJax_TypeWriter")
-add_cat_button.place(x = 280 , y = 10)
+add_cat_button = tk.Button(objectives , text= "Add" , command=add_cat , bg="#f57cab" , font="MathJax_TypeWriter")
+add_cat_button.place(x = 280 , y = 30)
 def add():
     item = category_list_combobox.get()
     index = list1.index(item)
@@ -91,14 +95,15 @@ def add():
     print(category_list[index].data)
     list_refresh()
     
-add_button = tk.Button(window , text= "Add" , command=add , bg="#f57cab" , font="MathJax_TypeWriter") 
-add_button.place(x= 220 , y =70)
+add_button = tk.Button(objectives , text= "Add" , command=add , bg="#f57cab" , font="MathJax_TypeWriter") 
+add_button.place(x= 220 , y =90)
 
-tk.Label(window , text = "====================================================================================").place(x=-2 , y=40)
-
-
+#--------====================== timer tab ===========-----------------
 
 
+timer_tab = tk.Frame(notebook , width= 600 , height= 400  ,bg="#ebbfd0")
+timer_tab.pack()
+notebook.add(timer_tab , text="timer"  )
 
 
 
